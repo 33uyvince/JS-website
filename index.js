280ecef7d7fee23554f0f80f0101ca8236c0a907
33uyvince <sed.engl.18@gmail.com>
2023-01-26 13:31:56 +0800

    added objects

---
+++ index.js
@@ -0,0 +1,16 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Ruric Thar",
+        Specialty: "Smash Blue",
+        img: "images/ruricthar.png",
+        text: "vigilance, reach. Ruric Thar unbowed attacks each combait if able. Whenever a player casts a noncreature spell, Ruric deals 6 damage to that player",
+    },
+    {
+        id: 2,
+        name: "Fynn",
+        Specialty: "Poison counters",
+        img: "images/fynn.png",
+        text: "Deathtouch. Whenever a creature you control with deathtouch deals combat damage to a player, thar player gets two poison counters.",
+    },
+];
